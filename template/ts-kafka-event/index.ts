db171731825a352ba740584b42dc70ee3847135c
Nils Perret <nils-frederic.perret@bos.no>
2022-01-11 23:32:28 +0100

    Add a simple HTTP server in order to provide an endpoint for kubernetes liveness

--- template/ts-kafka-event/index.ts
+++ template/ts-kafka-event/index.ts
@@ -1,9 +1,26 @@
+/** @format */
+
 // Copyright (c) Alex Ellis 2017. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-import { startKafkaConsumer } from "./function";
-import { getEventHubsVariables } from "./function/env-variables";
-import { IEnvironmentVariables } from "./function/types";
+import Fastify, { FastifyInstance } from 'fastify';
+import { startKafkaConsumer } from './function';
+import { getEventHubsVariables } from './function/env-variables';
+import { IEnvironmentVariables } from './function/types';
 
-getEventHubsVariables(process.env)
-  .then((variables: IEnvironmentVariables) => startKafkaConsumer(variables));
+getEventHubsVariables(process.env).then((variables: IEnvironmentVariables) => startKafkaConsumer(variables));
 
+const httpPort = process.env.http_port || 3000;
+
+const server: FastifyInstance = Fastify();
+
+server.get('/healthz', async (request: any, reply: any) => {
+  return { message: 'I am running!' };
+});
+
+server.listen(httpPort, (err: Error, address: string | number) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`🎉 HTTP server listening at ${address}`);
+});
