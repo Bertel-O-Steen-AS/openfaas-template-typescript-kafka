4d092105d19bed97b5597d4703c3362f86eaaf7b
Nils Perret <nils-frederic.perret@bos.no>
2022-03-09 22:43:03 +0100

    Change the Kafka Channel interface, since each function mostly consumes and produces on a different channel

--- template/ts-kafka-event/function/env-variables.ts
+++ template/ts-kafka-event/function/env-variables.ts
@@ -5,17 +5,17 @@ import { IEnvironmentVariables } from './types';
 const secretNames = {
   connectionString: '<ConnectionStringSecretName>',
   eventHubName: '<EventHubSecretName>',
-  evenHubNamespaceUrl: '<EvenHubNamespaceUrlSecretName>'
+  evenHubNamespaceUrl: '<EvenHubNamespaceUrlSecretName>',
 };
 
-export const getEventHubsVariables = async (
-  env = process.env
-): Promise<IEnvironmentVariables> => ({
+export const getEventHubsVariables = async (env = process.env): Promise<IEnvironmentVariables> => ({
   CONSUMER_GROUP: env?.CONSUMER_GROUP,
-  EVENT_HUB_CONNECTION_STRING: env?.EVENT_HUB_CONNECTION_STRING || (await getKubernetesSecret(secretNames.connectionString)),
+  EVENT_HUB_CONNECTION_STRING:
+    env?.EVENT_HUB_CONNECTION_STRING || (await getKubernetesSecret(secretNames.connectionString)),
   EVENT_HUB_NAME: env?.EVENT_HUB_NAME || (await getKubernetesSecret(secretNames.eventHubName)),
   EVENT_HUB_NAMESPACE_URL: env?.EVENT_HUB_NAMESPACE_URL || (await getKubernetesSecret(secretNames.evenHubNamespaceUrl)),
-  KAFKA_CHANNEL: env?.KAFKA_CHANNEL || null,
+  EVENT_HUB_KAFKA_CHANNEL_CONSUME: env?.EVENT_HUB_KAFKA_CHANNEL_CONSUME || null,
+  EVENT_HUB_KAFKA_CHANNEL_PRODUCE: env?.EVENT_HUB_KAFKA_CHANNEL_PRODUCE || null,
   KAFKA_PORT: env?.KAFKA_PORT,
   LOG_LEVEL: env?.LOG_LEVEL || 'info',
 });
--- template/ts-kafka-event/function/types.ts
+++ template/ts-kafka-event/function/types.ts
@@ -3,7 +3,8 @@ export interface IEnvironmentVariables {
   EVENT_HUB_CONNECTION_STRING: string;
   EVENT_HUB_NAME: string;
   EVENT_HUB_NAMESPACE_URL: string;
-  KAFKA_CHANNEL: string;
+  EVENT_HUB_KAFKA_CHANNEL_CONSUME: string;
+  EVENT_HUB_KAFKA_CHANNEL_PRODUCE: string;
   KAFKA_PORT: string;
   LOG_LEVEL: string;
 }
